f073abba8f31e3dbb928a38d8c8188b9175cd4da
irvansyachrudin <irvanbrow97@gmail.com>
2020-06-30 19:12:44 +0700

    commit pertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <font>
+        <h1>SanberBook</h1>
+        <h1>Social Media Developer Santai berkualitas</h1>
+
+        <p>belajar berbagi agar hidup ini semakin santai berkualitas</p>
+
+        <h4>Benefit Join di SamberBook </h4>
+        <ul>
+            <li>Mendapatkan motivasi sesama developer</li>
+            <li>Sharing Knowledge dengan para mastah Sanber</li>
+            <li>Dibuat oleh calon web dev terbaik</li>
+        </ul>
+
+        <h4>Cara bergabung SanberBook</h4>
+        <ol>
+            <li>Mengunjungi Web ini</li>
+            <li>Mendaftar di <a href="/register"> Form Sign Up</a></li>
+            <li>Selesai</li>
+        </ol>
+    </font>
+</body>
+
+</html>
